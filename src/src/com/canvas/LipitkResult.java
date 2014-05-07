7ae8ef1445f76a233a755832255f8d94e07916b5
Zhang Yungui <rhcad@hotmail.com>
2014-05-07 16:53:45 +0800

    lipi-toolkit4.0.0-android

---
+++ src/src/com/canvas/LipitkResult.java
@@ -0,0 +1,12 @@
+package com.canvas;
+
+public class LipitkResult {
+
+	public LipitkResult() {
+		Id = -1;
+		Confidence = 0;
+	}
+
+	public int Id;
+	public float Confidence;
+}
---
+++ src/src/com/canvas/Symbol.java
@@ -0,0 +1,25 @@
+package com.canvas;
+
+import java.util.List;
+
+import android.graphics.PointF;
+import android.graphics.Matrix;
+
+public class Symbol {
+	private Stroke[] _strokes;
+	private String _character;
+	
+	public Symbol(Stroke[] strokes, String character) {
+		_strokes = strokes;
+		_character = character;		
+	}
+	
+	public Stroke[] getStrokes() {
+		return _strokes;
+	}
+	
+	public String getCharacter() {
+		return _character;
+	}
+}
+
